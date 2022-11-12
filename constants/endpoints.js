567abd040814fd05b0b16879cd36a61473539aad
Ricardo Almeida <rcrdalmeida@outlook.pt>
2022-11-12 16:18:34 +0000

    feat(jobs): create jobs, jsdocs

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -1,5 +1,6 @@
 const SF_ENDPOINTS = {
-    login: '/services/oauth2/token'
+    login: '/services/oauth2/token',
+    createJob: '/services/data/v53.0/jobs/ingest'
 }
 
 module.exports = SF_ENDPOINTS;
